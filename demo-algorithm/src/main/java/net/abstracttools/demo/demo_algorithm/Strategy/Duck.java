bcb417b03ea28902f5fbeb3535f62ddd332bc6d1
godkill <godkill24@gmail.com>
2016-07-14 23:15:22 +0800

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit c249904dee4c4fd7bf83de278c317156b20185b2, reversing
    changes made to b3fa58ba3fa9c3da4bd07ded50f148d3738ba254.

--- demo-algorithm/src/main/java/net/abstracttools/demo/demo_algorithm/Strategy/Duck.java
+++ demo-algorithm/src/main/java/net/abstracttools/demo/demo_algorithm/Strategy/Duck.java
@@ -1,12 +1,5 @@
 package net.abstracttools.demo.demo_algorithm.Strategy;
 
-/**
- * 
- * 类名：Duck.java <br/>
- * 功能说明： <br/>
- * 修改历史： <br/>
- * 1.[2016年6月1日下午5:13:59]创建类 by Administrator
- */
 public abstract class Duck {
 
 }
